2dbb41ae0a540cb025c7ab84add9ec5dbaab1f14
Geert Kinthaert <gkinthaert@hotmail.com>
2026-03-03 15:35:33 -0500

    Add web server instance and security group configuration
    Added a web server EC2 instance with user data for Apache installation and a security group to allow HTTP traffic.

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-# setup of EC2 instance
+# setup of EC2 instance 
 
 resource "aws_instance" "example" {
   ami           = "ami-09256c524fab91d36"
@@ -13,4 +13,41 @@ resource "aws_instance" "example" {
   tags = {
     Name = "example-ec2-instance"
   }
+}
+
+# deploying a web server
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-09256c524fab91d36"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.web_server_sg.id]
+
+  tags = {
+    Name = "web-server-instance"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install apache2 -y
+              sudo systemctl start apache2
+              sudo systemctl enable apache2
+              echo "<h1>Welcome to the Web Server</h1>" > /var/www/html/index.html
+              EOF
+  user_data_replace_on_change = true
+
+}
+
+# security group for the web server
+
+resource "aws_security_group" "web_server_sg" {
+  name        = "web-server-sg"
+  description = "Allow HTTP traffic to web server"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
 }
